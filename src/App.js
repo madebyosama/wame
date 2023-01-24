417b9cf82428985d01d5df7a46f465830d1fb084
codebyosama <codebyosama@gmail.com>
2023-01-24 21:04:13 +0500

    Deployment ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import Logo from './assets/whatsapp-logo.svg';
+
+export default function App() {
+  const [num, setNum] = useState(123456789);
+  function redirect(e) {
+    e.preventDefault();
+    window.open(
+      `https://web.whatsapp.com/send/?phone=%2B${num.replace(
+        /\D/g,
+        ''
+      )}&text&type=phone_number&app_absent=0`
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <img className='hero-logo' src={Logo} width='64px' />
+      <h1 className='heading'>
+        Send WhatsApp <br />
+        without Saving Contact
+      </h1>
+      <form onSubmit={(e) => redirect(e)}>
+        <input
+          autoFocus
+          className='number'
+          placeholder='Enter number'
+          type='text'
+          onChange={(e) => {
+            setNum(e.target.value);
+          }}
+        />
+        <span className='button'>
+          <input type='submit' className='submit' value='Message' />
+          <img className='logo' src={Logo} width='20px' />
+        </span>
+      </form>
     </div>
   );
 }
-
-export default App;
